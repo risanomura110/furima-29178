075d0a90fbb7729cc3522db3e53e10e27dc589b1
risanomura110 <aiueo110boy@gmail.com>
2020-08-31 00:38:25 +0900

    validatesの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :birthday, presence: true 
+  validates :email,  inclusion: { in: %w(@) }
+  validates :password,
+  format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :family_kanji, :first_kanji, presence: true 
+  validates :family_kanji, :first_kanji,
+  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :family_kana, :first_kana, presence: true 
+  validates :family_kana, :first_kana,
+  format: { with: /\A[ァ-ン]+\z/ }
 end
