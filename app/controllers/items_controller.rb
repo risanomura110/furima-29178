528c9fc75cc86e97f0636caccb2e0b6a1f320e0d
risanomura110 <aiueo110boy@gmail.com>
2020-08-31 23:59:02 +0900

    items#indexからitems#newに飛ぶ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in, except: [:index]
   def index
+  end
+  def new
+  end
+  def create
+  end
+  def destroy
+  end
+  def edit
+  end
+  def update
+  end
+  def show
+  end
+  private
+  def move_to_signed_in
+    unless user_signed_in?
+      #サインインしていないユーザーはログインページが表示される
+      redirect_to  '/users/sign_in'
+    end
   end
 end
