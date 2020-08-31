67eb97663d878262dba996bbf42a920513573576
risanomura110 <aiueo110boy@gmail.com>
2020-08-31 17:08:11 +0900

    Revert "間違えた"
    This reverts commit 3217034aa76099487777f3739fc594d48ce9b3a1.

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
    devise_for :users
 
   root  'items#index'
-    get  '/users'  to: 'devise/registrations#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items, only: :index
 end
