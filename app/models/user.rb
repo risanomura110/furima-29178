2d0f4ac232cb5e65a047f0f74ccbfeb759de60a6
risanomura110 <aiueo110boy@gmail.com>
2020-08-31 12:40:31 +0900

    訂正#1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :birthday, presence: true
-  # validates :email,  inclusion: { in: ["@"] }←デフォルトで"@"が含まれないと保存できない
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password,
-            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+            format: { with: PASSWORD_REGEX }
   validates :family_kanji, :first_kanji, presence: true
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
   validates :family_kanji, :first_kanji,
-            format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+            format: { with: NAME_REGEX }
   validates :family_kana, :first_kana, presence: true
+  NAMEKANA_REGEX = /\A[ァ-ン]+\z/.freeze
   validates :family_kana, :first_kana,
-            format: { with: /\A[ァ-ン]+\z/ }
+            format: { with: NAMEKANA_REGEX }
 end
