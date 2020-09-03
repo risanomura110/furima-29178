d6aa74724b7ec0f7eaf45218b5174c7690bbc272
risanomura110 <aiueo110boy@gmail.com>
2020-09-03 20:24:35 +0900

    turbolinksを消す

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()過去開いたページを記憶し,それをいち早く表示する
 require("@rails/activestorage").start()
 require("channels")
 require("../change")
