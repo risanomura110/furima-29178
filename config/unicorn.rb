276e5ee0778eb0fea32d00c7a1bdc17e73cfc41b
risanomura110 <aiueo110boy@gmail.com>
2020-09-14 18:03:30 +0900

    自動デプロイ

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,23 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+# 「../」が一つ増えている
+app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+# 「current」を指定
+working_directory "#{app_path}/current"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+# 「shared」の中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
-#ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+# 「shared」の中を参照するよう変更
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# 「shared」の中を参照するよう変更
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+
+# 「shared」の中を参照するよう変更
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
@@ -52,3 +53,43 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
+
+
+
+upstream app_server {
+  # Unicornと連携させるための設定
+  server unix:/var/www/furima-29178/shared/tmp/sockets/unicorn.sock;
+}
+
+# {}で囲った部分をブロックと呼ぶ。サーバの設定ができる
+server {
+  # このプログラムが接続を受け付けるポート番号
+  listen 80;
+  # 接続を受け付けるリクエストURL ここに書いていないURLではアクセスできない
+  server_name Elastic IP;
+
+  # クライアントからアップロードされてくるファイルの容量の上限を2ギガに設定。デフォルトは1メガなので大きめにしておく
+  client_max_body_size 2g;
+
+# 接続が来た際のrootディレクトリ
+  root /var/www/furima-29178/current/public;
+
+# assetsファイル(CSSやJavaScriptのファイルなど)にアクセスが来た際に適用される設定
+  location ^~ /assets/ {
+    gzip_static on;
+    expires max;
+    add_header Cache-Control public;
+    root /var/www/furima-29178/current/public;
+  }
+
+  try_files $uri/index.html $uri @unicorn;
+
+  location @unicorn {
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host $http_host;
+    proxy_redirect off;
+    proxy_pass http://app_server;
+  }
+
+  error_page 500 502 503 504 /500.html;
+}
