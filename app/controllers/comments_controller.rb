082dc0f9606b3501ed10d24493c0d1717afc1c0e
risanomura110 <aiueo110boy@gmail.com>
2020-09-15 18:04:03 +0900

    Revert "間違え"
    This reverts commit 74566dbd23c53b426a062f545cf6a0b4cce8c8b4.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,7 @@
 class CommentsController < ApplicationController
-  def new
-    @comments = Comment.all
-    @comment = Comment.new
-  end
-
   def create
-    @comment = Comment.new(comment_params)
-    if @comment.save
-      ActionCable.server.broadcast 'message_channel', content: @comment
-    end
+    comment = Comment.create(comment_params)
+    redirect_to "/items/#{comment.item.id}" # コメントと結びつくツイートの詳細画面に遷移する
   end
 
   private
